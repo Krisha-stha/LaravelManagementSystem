edbe7672a33c93e7b462baddf25f7caee22d3d3b
Krisha Shrestha <kirishshaaa@gmail.com>
2025-12-17 11:18:13 +0000

    admin middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -15,6 +15,9 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if(Auth::check()&&Auth::user()->usertype=="admin"){
+            return $next($request);
+        }
+        abort(401, 'Unauthorized access!');
     }
 }
